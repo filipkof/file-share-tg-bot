40a6dd6b9578647ce93e5f23f0eb1fbbee5ec200
filipkof <71509204+svegoe@users.noreply.github.com>
2024-08-24 22:07:44 +0300

    Initial commit

---
+++ src/main/java/com/example/filesharetgbot/ShareBot.java
@@ -0,0 +1,4 @@
+package com.example.filesharetgbot;
+
+public class ShareBot {
+}
---
+++ src/test/java/com/example/filesharetgbot/FileShareTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filesharetgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileShareTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
