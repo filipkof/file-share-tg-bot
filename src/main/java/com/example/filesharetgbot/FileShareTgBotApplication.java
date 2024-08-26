8a2ecdcaeb2b48c0408707ec4c5fbbbc7e59fd45
filipkof <71509204+svegoe@users.noreply.github.com>
2024-08-26 22:11:35 +0300

    actual version

--- src/main/java/com/example/filesharetgbot/FileShareTgBotApplication.java
+++ src/main/java/com/example/filesharetgbot/FileShareTgBotApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FileShareTgBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileShareTgBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FileShareTgBotApplication.class, args);
+    }
 
 }
